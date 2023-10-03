b8bc097a81389d2ae82d137cd786645f156cc7d4
arshia <arshia.karimi@gmail.com>
2023-10-03 15:10:46 +0330

    qr code update

--- QR-Code.js
+++ QR-Code.js
@@ -1,15 +1,15 @@
-const generateBtn = document.querySelector('form button');
-const qrCodeBox = document.querySelector('.qr-code');
-const qrInput = document.querySelector('form input');
-const qrImge = document.querySelector('.qr-code img');
+const generateBtn = document.querySelector("form button");
+const qrCodeBox = document.querySelector(".qr-code");
+const qrInput = document.querySelector("form input");
+const qrImge = document.querySelector(".qr-code img");
 
-generateBtn.addEventListener('click' , (event)=>{
-    event.preventDefault()
-    let qrValue = qrInput.value;
-    if(!qrValue){
-        return alert('لطفا یک ادرس یا یک متن را وارد کنید.')
-    }else {
-        qrCodeBox.classList.remove('hidden');
-        qrImge.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`
-    }
-})
+generateBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let qrValue = qrInput.value;
+  if (!qrValue) {
+    return alert("لطفا یک ادرس یا یک متن را وارد کنید.");
+  } else {
+    qrCodeBox.classList.remove("hidden");
+    qrImge.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+  }
+});
